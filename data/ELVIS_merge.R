6f16772b0de7f5275f3f9c9d772fa327fbdbfb55
sandervkuijk <sander1981@gmail.com>
2023-04-03 16:10:37 +0200

    Elvis version March

--- data/ELVIS_merge.R
+++ data/ELVIS_merge.R
@@ -42,7 +42,7 @@ library(sjmisc)
 setwd("L:/SCEN/PZ-KEMTA/PROJECTEN/CORFU/Data/WERK DATA cohorten/ELVIS/dataset archief")
 full <- openxlsx::read.xlsx("ELVIS_acute en vragenlijst data extractie van 26.01.2023.xlsx",
                             sheet = "Export to MUMC")
-full$TrFU_AdmDtNW <- as.Date(full$TrFU_AdmDtNW, origin = "1899-12-30")
+full$admission_hospital_date <- as.Date(full$admission_hospital_date, origin = "1899-12-30")
 full$proven_infection_date <- as.Date(full$proven_infection_date, origin = "1899-12-30")
 
 full$INGEVULD   <- as.Date(full$INGEVULD, origin = "1899-12-30")
@@ -53,7 +53,7 @@ full <- full[, colSums(is.na(full)) < nrow(full)]
 
 first <- full[, c(1:125, 135:348)] 
 first$months <- as.numeric(round(difftime(first$INGEVULD,
-                                 first$TrFU_AdmDtNW, units = "weeks")/(52/12)))
+                                 first$admission_hospital_date, units = "weeks")/(52/12)))
 first$moment <- ifelse(first$months < 9, 6,
                        ifelse(first$months < 15, 12,
                               ifelse(first$months < 21, 18, 24)))
@@ -66,7 +66,7 @@ first$moment <- ifelse(first$months < 9, 6,
 ## Second round of questionnaires
 second <- full[, c(1:125, 351:524)]
 second$months <- as.numeric(round(difftime(second$INGEVULD.1,
-                            second$TrFU_AdmDtNW, units = "weeks")/(52/12)))
+                            second$admission_hospital_date, units = "weeks")/(52/12)))
 second$moment <- ifelse(second$months < 9, 6,
                        ifelse(second$months < 15, 12,
                               ifelse(second$months < 21, 18, 24)))
@@ -74,10 +74,11 @@ second$moment <- ifelse(second$months < 9, 6,
 ## Third round of questionnaires
 third <- full[, c(1:125, 527:718)]
 third$months <- as.numeric(round(difftime(third$INGEVULD.2,
-                           third$TrFU_AdmDtNW, units = "weeks")/(52/12)))
+                           third$admission_hospital_date, units = "weeks")/(52/12)))
 third$moment <- ifelse(third$months < 9, 6,
                         ifelse(third$months < 15, 12,
                                ifelse(third$months < 21, 18, 24)))
+rm(full)
 
 ## Merge to long format dataframe before going wide
 # Make sure second and third questionnaires are of same length
@@ -86,11 +87,64 @@ ch_sec <- names(second[, grep(".1", names(second), fixed = TRUE, value = FALSE)]
 names(second)[grep(".1", names(second), fixed = TRUE, value = FALSE)] <-
 substr(ch_sec, 1, nchar(ch_sec)-2)
 
-ch_thi1  <- names(third[, grep(".1", names(third), fixed = TRUE, value = FALSE)])
+ch_thi1 <- names(third[, grep(".1", names(third), fixed = TRUE, value = FALSE)])
 ch_thi2 <- names(third[, grep(".2", names(third), fixed = TRUE, value = FALSE)])
 names(third)[grep(".1", names(third), fixed = TRUE, value = FALSE)] <-
   substr(ch_thi1, 1, nchar(ch_thi1)-2)
 names(third)[grep(".2", names(third), fixed = TRUE, value = FALSE)] <-
   substr(ch_thi2, 1, nchar(ch_thi2)-2)
 
-setdiff(names())
+rm(ch_sec, ch_thi1, ch_thi2)
+
+# Fill non-existing columns in combined data.frames with NA's
+first[setdiff(names(second), names(first))] <- NA
+second[setdiff(names(first), names(second))] <- NA
+TEMP <- rbind(first, second)
+
+TEMP[setdiff(names(third), names(TEMP))] <- NA
+third[setdiff(names(TEMP), names(third))] <- NA
+all.data.long <- rbind(TEMP, third)
+all.data.long <- subset(all.data.long, !is.na(all.data.long$moment))
+rm(first, second, third, TEMP)
+
+# long to wide
+all.data.long <- all.data.long[!(duplicated(all.data.long[c("Cohort.ID","moment")],
+                                            fromLast = FALSE)), ]
+all.data.wide <- reshape(all.data.long, idvar = "Cohort.ID",
+                         v.names = c("INGEVULD", "code", "months",
+                                     grep("C1", names(all.data.long), value = TRUE)),
+                         timevar = "moment", direction = "wide")
+# rm(all.data.long) # May be useful for longitudinal analyses
+
+names(all.data.wide) <- gsub(x = names(all.data.wide), pattern = ".6", replacement = "_6mdn") 
+names(all.data.wide) <- gsub(x = names(all.data.wide), pattern = ".12", replacement = "_12mdn") 
+names(all.data.wide) <- gsub(x = names(all.data.wide), pattern = ".18", replacement = "_18mdn") 
+names(all.data.wide) <- gsub(x = names(all.data.wide), pattern = ".24", replacement = "_24mdn") 
+
+all.data <- all.data.wide[, c(1:125,
+                          grep("_6mdn", names(all.data.wide)),
+                          grep("_12mdn", names(all.data.wide)),
+                          grep("_18mdn", names(all.data.wide)),
+                          grep("_24mdn", names(all.data.wide)))]
+all.data$`Any.malignancy.(Hematological.or.neoplasm)` <- NULL
+
+# Shorten strings with input that is too long.
+all.data$C1ENQOTH_6mdn  <- substr(all.data$C1ENQOTH_6mdn, 1, 8)
+all.data$C1ENQOTH_12mdn <- substr(all.data$C1ENQOTH_12mdn, 1, 8)
+all.data$C1ENQOTH_18mdn <- substr(all.data$C1ENQOTH_18mdn, 1, 8)
+all.data$C1ENQOTH_24mdn <- substr(all.data$C1ENQOTH_24mdn, 1, 8)
+
+all.data$C1HLPALL_X_6mdn  <- substr(all.data$C1HLPALL_X_6mdn, 1, 8)
+all.data$C1HLPALL_X_12mdn <- substr(all.data$C1HLPALL_X_12mdn, 1, 8)
+all.data$C1HLPALL_X_18mdn <- substr(all.data$C1HLPALL_X_18mdn, 1, 8)
+all.data$C1HLPALL__24mdn  <- substr(all.data$C1HLPALL__24mdn, 1, 8)
+
+all.data$C1OTHER_X_6mdn   <- substr(all.data$C1OTHER_X_6mdn, 1, 8)
+all.data$C1OTHER_X_12mdn  <- substr(all.data$C1OTHER_X_12mdn, 1, 8)
+all.data$C1OTHER_X_18mdn  <- substr(all.data$C1OTHER_X_18mdn, 1, 8)
+all.data$C1OTHER_X_24mdn  <- substr(all.data$C1OTHER_X_24mdn, 1, 8)
+
+setwd("L:/SCEN/PZ-KEMTA/PROJECTEN/CORFU/Data/WERK DATA cohorten/ELVIS/TOTAAL")
+write_sav(all.data, "ELVIS_CORFU_NOT_FINAL.sav")
+
+### End of file.
